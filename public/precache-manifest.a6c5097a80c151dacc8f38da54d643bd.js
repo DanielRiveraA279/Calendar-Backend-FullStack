0946473ed61f4f2e7563e7370c8204d14fed4d24
DanielRiveraA279 <danielriveraruta3@gmail.com>
2022-04-07 08:15:10 -0300

    v1 CalendarApp

---
+++ public/precache-manifest.a6c5097a80c151dacc8f38da54d643bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df494c45563a33c6a532a0e6e8d82d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f336295b117ea3672fd6",
+    "url": "/static/css/2.d0f3d0f8.chunk.css"
+  },
+  {
+    "revision": "ca87273ecdf3a77164e1",
+    "url": "/static/css/main.b011ef85.chunk.css"
+  },
+  {
+    "revision": "f336295b117ea3672fd6",
+    "url": "/static/js/2.f2739028.chunk.js"
+  },
+  {
+    "revision": "ca87273ecdf3a77164e1",
+    "url": "/static/js/main.a09eeb35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
